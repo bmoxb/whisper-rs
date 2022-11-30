0e8611ef399409a6c92ca6424d32387f82b50491
bmoxb <maxoblack@yahoo.com>
2022-11-30 18:29:15 +0000

    call Whisper via Python from Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+
+fn testing123() -> PyResult<String> {
+    pyo3::prepare_freethreaded_python();
+
+    Python::with_gil(|py| {
+        let os = PyModule::import(py, "os")?;
+        let s: &str = os.getattr("getcwd")?.call0()?.extract()?;
+        println!("{}", s);
+
+        let whisper = PyModule::import(py, "whisper")?;
+
+        let args = ("base",);
+        let model = whisper.getattr("load_model")?.call1(args)?;
+        let result: &PyDict = model.call_method1("transcribe", ("audio.mp3",))?.downcast()?;
+
+        result.get_item("text").unwrap().extract()
+    })
 }
 
 #[cfg(test)]
@@ -7,8 +24,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn my_test() {
+    assert_eq!(testing123().unwrap(), " Hello world.");
     }
 }
