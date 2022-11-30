04987ca977da4b020cd1c28a8924a0627e0cb386
bmoxb <maxoblack@yahoo.com>
2022-11-30 22:19:44 +0000

    collect segments of text with timestamps

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,10 @@
-use std::convert::AsRef;
+use std::convert::{AsRef, TryFrom};
 use std::path::Path;
 
 use crate::Language;
 
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
+use pyo3::types::*;
 
 pub struct Model {
     model: Py<PyAny>,
@@ -25,7 +25,7 @@ impl Model {
     pub fn transcribe(
         &self,
         path: impl AsRef<Path>,
-        language: Language,
+        language: Option<Language>,
     ) -> Result<Transcription, PyErr> {
         let full_path_string = path.as_ref().canonicalize().unwrap().display().to_string();
 
@@ -33,16 +33,15 @@ impl Model {
             let model = self.model.as_ref(py);
 
             let kwargs = PyDict::new(py);
-            kwargs.set_item("language", language.to_string())?;
+            if let Some(language) = language {
+                kwargs.set_item("language", language.to_string())?;
+            }
 
             let dict: &PyDict = model
                 .call_method("transcribe", (full_path_string,), Some(kwargs))?
                 .downcast()?;
 
-            Ok(Transcription {
-                text: dict.get_item("text").unwrap().extract()?,
-                language,
-            })
+            dict.try_into()
         })
     }
 }
@@ -60,5 +59,48 @@ pub enum ModelSize {
 #[derive(Clone, Debug)]
 pub struct Transcription {
     pub text: String,
+    pub segments: Vec<Segment>,
     pub language: Language,
+}
+
+impl TryFrom<&PyDict> for Transcription {
+    type Error = PyErr;
+
+    fn try_from(dict: &PyDict) -> Result<Self, Self::Error> {
+        let text = dict.get_item("text").unwrap().extract()?;
+
+        let segments = dict
+            .get_item("segments")
+            .unwrap()
+            .iter()?
+            .map(|s| {
+                let dict: &PyDict = s?.downcast()?;
+                Ok(Segment {
+                    text: dict.get_item("text").unwrap().extract()?,
+                    start: dict.get_item("start").unwrap().extract()?,
+                    end: dict.get_item("end").unwrap().extract()?,
+                })
+            })
+            .collect::<Result<Vec<Segment>, PyErr>>()?;
+
+        let language = dict
+            .get_item("language")
+            .unwrap()
+            .extract::<&str>()?
+            .try_into()
+            .unwrap();
+
+        Ok(Transcription {
+            text,
+            segments,
+            language,
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Segment {
+    pub text: String,
+    pub start: f32,
+    pub end: f32,
 }
